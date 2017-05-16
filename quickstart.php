000a5bd0082e4652b1a765c1a907a912e8b8f9ee
Ethan Mallove <mallove@fas.harvard.edu>
2017-05-16 14:10:11 -0400

    Clean up test calendars. Rename credentials file.

--- quickstart.php
+++ quickstart.php
@@ -6,7 +6,7 @@ define('APPLICATION_NAME', 'Google Calendar API PHP Quickstart');
 
 # EAM: WHAT'S THIS?
 # define('CREDENTIALS_PATH', '~/.credentials/calendar-php-quickstart.json');
-define('CREDENTIALS_PATH', '/Users/hmdcadministrator/Downloads/APIProject-a4722b82e133.json.FILE_DOES_NOT_EXIST');
+define('CREDENTIALS_PATH', '/Users/hmdcadministrator/Downloads/APIProject-a4722b82e133.json');
 define('CLIENT_SECRET_PATH', '/Users/hmdcadministrator/php-oauth2-example/client_secret_631256725070-1voucmae0h2fs9ej0rc2hgol084lgead.apps.googleusercontent.com.json');
 
 // If modifying these scopes, delete your previously saved credentials
@@ -15,10 +15,8 @@ define('SCOPES', implode(' ', array(
   Google_Service_Calendar::CALENDAR )
 ));
 
-
 # $redirectUri = 'http://localhost:8080/alldone.php';
 $redirectUri = 'https://oauth-redirect.googleusercontent.com/r/core-stronghold-491';
-
 
 if (php_sapi_name() != 'cli') {
   throw new Exception('This application must be run on the command line.');
@@ -85,7 +83,7 @@ function expandHomeDirectory($path) {
 // Get the API client and construct the service object.
 $client = getClient();
 $client->setRedirectUri($redirectUri);
-$service = new Google_Service_Calendar($client);
+$calendarList = $service->calendarList;
 
 // Print the next 10 events on the user's calendar.
 $calendarId = 'primary';
@@ -96,15 +94,16 @@ $optParams = array(
   'timeMin' => date('c'),
 );
 
-# $results = $service->events->listEvents($calendarId, $optParams);
 $calendarService = new Google_Service_Calendar($client);
+
+# Create a test calendar
 $calendar_calendar = new Google_Service_Calendar_Calendar();
 $calendar_calendar->setDescription("EAM 1002 Description");
 $calendar_calendar->setSummary("EAM 1002 Summary");
 $calendar = $calendarService->calendars->insert($calendar_calendar);
 
+# Create test event and add it to the calendar
 $event = new Google_Service_Calendar_Event();
-
 $dt1 = new DateTime("2017-05-16 05:00:00");
 $dt2 = new DateTime("2017-05-16 07:00:00");
 $calendarDateTime = new Google_Service_Calendar_EventDateTime();
@@ -115,16 +114,42 @@ $event->setEnd($calendarDateTime);
 $event->setDescription("Felipe's tacos");
 $calendarService->events->insert($calendar->getId(), $event);
 
-if (count($results->getItems()) == 0) {
-  print "No upcoming events found.\n";
-} else {
-  print "Upcoming events:\n";
-  foreach ($results->getItems() as $event) {
-    $start = $event->start->dateTime;
-    if (empty($start)) {
-      $start = $event->start->date;
-    }
-    printf("%s (%s)\n", $event->getSummary(), $start);
-  }
-}
+# List events to verify this is the one we want to delete
+# $l = $calendarList->listCalendarList();
+# var_dump($l);
+# $calendarId = "u9t25rgktomt5s98lkv49cmon0@group.calendar.google.com";
+# $r = $service->events->listEvents($calendarId, $optParams);
+# var_dump($r);
 
+#########################
+#
+# Clean up test calendars
+#
+#########################
+
+
+# $calendarIdsToDelete = array(
+#   "u9t25rgktomt5s98lkv49cmon0@group.calendar.google.com",
+#   "v7nja0am0d9qdm524iuaeu3t0c@group.calendar.google.com",
+#   "ktkvnni9k7p2iv4lct7kkr0sp8@group.calendar.google.com",
+#   "m2dvpfgu4ufp8n5mtvhsdlg4us@group.calendar.google.com",
+#   "ac7rpujj9mc906r34rk3i741e4@group.calendar.google.com",
+#   "rcda73oii0c2pd15eq3uppvjk0@group.calendar.google.com",
+#   "ot72mtphkrm36q9b6hobm3ftlc@group.calendar.google.com",
+#   "0k85ucandjl676lbe9j5bgs5o4@group.calendar.google.com",
+#   "tepvdd9r7kqprvb6iquag32nss@group.calendar.google.com",
+#   "1ce3jr3lji4ncicni7nqi83ljo@group.calendar.google.com",
+#   "qb0bm0sdk117756ng81e5n0vp0@group.calendar.google.com",
+#   "lgbnv4244q4add89j91k9fsjbg@group.calendar.google.com"
+# );
+# 
+# 
+# foreach ($calendarIdsToDelete as $i) {
+#   try {
+#     $calendarService->calendars->delete($i);
+#   } catch (Exception $e) {
+#     print "e = " . var_export($e, true);
+#   }
+# }
+# 
+# var_dump($r);
